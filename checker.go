3c66a9f7fbc931be698fe1cfb3818c1e86abcec6
Ayomide-codex <ayshowdaniel@gmail.com>
2026-07-02 15:35:37 +0100

    add: checker.go with findClosest function

--- checker.go
+++ checker.go
@@ -2,7 +2,7 @@ package main
 
 func findClosest(word string, dictionary []string) string {
 	for _, dictWord := range dictionary {
-		if isSimilar(word, dictionary) {
+		if isSimilar(word, dictWord) {
 			return dictWord
 		}
 	}
